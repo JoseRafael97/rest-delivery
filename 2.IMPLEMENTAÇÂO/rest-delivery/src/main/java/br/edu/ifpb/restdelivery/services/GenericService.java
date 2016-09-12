29a3241cbaa247a3097a3f90d8786f6dc1411021
JoseRafael97 <jose.rafael.feitosa@gmail.com>
2016-09-12 07:46:33 -0300

    adicionado service generico

---
+++ 2.IMPLEMENTAÇÂO/rest-delivery/src/main/java/br/edu/ifpb/restdelivery/services/GenericService.java
@@ -0,0 +1,65 @@
+package br.edu.ifpb.restdelivery.services;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.validation.constraints.NotNull;
+
+import br.edu.ifpb.restdelivery.exceptions.RestDeliveryException;
+import br.edu.ifpb.restdelivery.exceptions.RestDeliveryPersistenceException;
+
+/**
+ * 
+ * @author rafaelfeitosa - <a href="https://github.com/JoseRafael97 ></a>
+ *
+ * @param <T> Tipo da classe.
+ * @param <K> Tipo do identificador.
+ */
+
+public interface GenericService<T, K extends Serializable> {
+
+	/**
+	 * Salva objetos de acordo com o tipo passado
+	 * 
+	 * @param obj
+	 *            - Entidade ao qual deseja salvar.
+	 */
+	
+	public void save(@NotNull T obj) throws RestDeliveryException;
+
+	/**
+	 * Atualiza dados de um objeto de acordo com o tipo passado
+	 * 
+	 * @param obj
+	 *            Entidade que se deseja atualizar
+	 */
+	
+	
+	public void update(@NotNull T obj) throws RestDeliveryException;
+
+	/**
+	 * Remove objetos do Banco de dados de acordo com o tipo passado
+	 * 
+	 * @param ob
+	 *            - Entidade ao qual deseja remover.
+	 */
+	public void remove(@NotNull T obj) throws RestDeliveryException;
+
+	/**
+	 * Busca objetos de acordo com a chave passada e com o tipo de objeto
+	 * requisitado
+	 * 
+	 * @param id
+	 *            - Identificador do objeto buscado
+	 * @return O objeto buscado.
+	 */
+	public T find(@NotNull K id) throws RestDeliveryException;
+
+	/**
+	 * Busca todos os objetos existente do tipo requisitado.
+	 * 
+	 * @return Lista de objetos do tipo da Entidade requisitada.
+	 * @throws RestDeliveryPersistenceException 
+	 */
+	public List<T> listAll() throws RestDeliveryPersistenceException;
+}
---
+++ 2.IMPLEMENTAÇÂO/rest-delivery/src/main/java/br/edu/ifpb/restdelivery/services/impl/ImplGenericService.java
@@ -0,0 +1,72 @@
+package br.edu.ifpb.restdelivery.services.impl;
+
+import java.io.Serializable;
+import java.util.List;
+
+import br.edu.ifpb.restdelivery.dao.GenericDAO;
+import br.edu.ifpb.restdelivery.exceptions.RestDeliveryException;
+import br.edu.ifpb.restdelivery.exceptions.RestDeliveryPersistenceException;
+import br.edu.ifpb.restdelivery.services.GenericService;
+import br.edu.ifpb.restdelivery.util.jpa.Transactional;
+
+/**
+ * Implementação da interface do GenericService 
+ * 
+ * @author rafaelfeitosa - <a href="https://github.com/JoseRafael97 ></a>
+ *
+ * @param <T> - Tipo da classe
+ * @param <K> - tipo do identificador
+ */
+public class ImplGenericService<T, K extends Serializable> implements GenericService<T, K>, Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	protected GenericDAO<T, K> dao;
+
+	/**
+	 * Método que salva o objeto no banco de dados.
+	 */
+	@Transactional
+	public void save(T obj) throws RestDeliveryException {
+		dao.add(obj);
+	}
+
+
+	/**
+	 * Método atualiza o objeto com as informações do objeto passado no parâmetro
+	 */
+	@Override
+	@Transactional
+	public void update(T obj) throws RestDeliveryException {
+		dao.update(obj);
+	}
+
+	/**
+	 * Método que remove o objeto do banco de dados de acordo com objeto passado no parâmetro
+	 */
+	@Override
+	@Transactional
+	public void remove(T obj) throws RestDeliveryException {
+		dao.remove(obj);
+	}
+
+	/**
+	 * Método que busca um objeto de acordo com o identificador passado no parâmetro
+	 */
+	@Override
+	public T find(K id) throws RestDeliveryException {
+		return dao.find(id);
+	}
+
+	/**
+	 * Método que busca todos os objetos do tipo da classe
+	 */
+	@Override
+	public List<T> listAll() throws RestDeliveryPersistenceException {
+		return dao.findAll();
+	}
+
+}
