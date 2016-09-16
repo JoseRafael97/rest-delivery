66bdb031f47a8e50e54790b72ba8dcf48991b032
JoseRafael97 <jose.rafael.feitosa@gmail.com>
2016-09-16 13:49:05 -0300

    correçẽos

---
+++ 2.IMPLEMENTAÇÂO/rest-delivery/src/main/java/br/edu/ifpb/restdelivery/converters/AddressConverter.java
@@ -0,0 +1,55 @@
+package br.edu.ifpb.restdelivery.converters;
+
+import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.ConverterException;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import br.edu.ifpb.restdelivery.entities.Address;
+import br.edu.ifpb.restdelivery.exceptions.RestDeliveryException;
+import br.edu.ifpb.restdelivery.services.impl.AddressService;
+
+@Named
+@RequestScoped
+public class AddressConverter implements Converter {
+
+	@Inject
+	private AddressService addressService;
+
+	@Override
+	public Object getAsObject(FacesContext context, UIComponent component, String value) {
+
+		if (value == null || value.trim().isEmpty()) {
+			return null;
+		}
+
+		Long id = Long.parseLong(value);
+
+		try {
+			return addressService.find(id);
+		} catch (RestDeliveryException e) {
+			String msgErroStr = String.format(
+					"Erro de conversão! Não foi possível realizar a conversão da string '%s' para o tipo esperado.",
+					value);
+			FacesMessage msgErro = new FacesMessage(FacesMessage.SEVERITY_ERROR, msgErroStr, msgErroStr);
+			throw new ConverterException(msgErro);
+
+		}
+
+	}
+
+	@Override
+	public String getAsString(FacesContext context, UIComponent component, Object value) {
+		if (value == null) {
+			return null;
+		}
+		Long id = ((Address) value).getId();
+
+		return (id != null) ? id.toString() : null;
+	}
+
+}
---
+++ 2.IMPLEMENTAÇÂO/rest-delivery/src/main/java/br/edu/ifpb/restdelivery/converters/OrderConverter.java
@@ -0,0 +1,56 @@
+package br.edu.ifpb.restdelivery.converters;
+
+import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.ConverterException;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import br.edu.ifpb.restdelivery.entities.Order;
+import br.edu.ifpb.restdelivery.exceptions.RestDeliveryException;
+import br.edu.ifpb.restdelivery.services.impl.OrderService;
+
+@Named
+@RequestScoped
+public class OrderConverter implements Converter{
+
+	@Inject
+	private OrderService service;
+
+	@Override
+	public Object getAsObject(FacesContext context, UIComponent component, String value) {
+
+		if (value == null || value.trim().isEmpty()) {
+			return null;
+		}
+
+		Long id = Long.parseLong(value);
+
+		try {
+			return service.find(id);
+		} catch (RestDeliveryException e) {
+			String msgErroStr = String.format(
+					"Erro de conversão! Não foi possível realizar a conversão da string '%s' para o tipo esperado.",
+					value);
+			FacesMessage msgErro = new FacesMessage(FacesMessage.SEVERITY_ERROR, msgErroStr, msgErroStr);
+			throw new ConverterException(msgErro);
+
+		}
+
+	}
+
+	@Override
+	public String getAsString(FacesContext context, UIComponent component, Object value) {
+		if (value == null) {
+			return null;
+		}
+		Long id = ((Order) value).getId();
+
+		return (id != null) ? id.toString() : null;
+
+	}
+
+}
